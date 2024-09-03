3f94997a036709be978ca4a76d2b4668c850d370
Miguel Buzato <miguel@mtbuzato.com.br>
2024-09-02 23:22:09 -0300

    feat: initial boilerplate

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'br.unicamp.ic.boilerplate',
+	appName: 'boilerplate',
+	webDir: 'dist',
+};
+
+export default config;
