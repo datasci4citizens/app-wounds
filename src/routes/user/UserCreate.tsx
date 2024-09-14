180f6c54579409686aa17b976e2f818b822d8995
André Santanchè <santanche@gmail.com>
2024-09-14 08:37:22 -0300

    fix (routes/user): form format adjusted

--- src/routes/user/UserCreate.tsx
+++ src/routes/user/UserCreate.tsx
@@ -2,6 +2,13 @@ import '../../globals.css'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 
+import { format } from 'date-fns'
+import { cn } from '../../lib/utils.ts'
+
+import { Card,
+         CardHeader,
+         CardTitle,
+         CardContent } from "@/components/ui/card";
 import { Input } from '../../components/ui/input.tsx'
 import { Switch } from '../../components/ui/switch.tsx'
 import { Select,
@@ -11,6 +18,11 @@ import { Select,
          SelectValue } from '../../components/ui/select.tsx'
 import { Textarea } from '../../components/ui/textarea.tsx'
 import { Button } from '../../components/ui/button.tsx'
+import { Popover,
+         PopoverContent,
+         PopoverTrigger } from '../../components/ui/popover.tsx'
+import { Calendar } from '../../components/ui/calendar.tsx'
+import { Calendar as CalendarIcon } from "lucide-react"
 
 import { useForm } from 'react-hook-form'
 import {
@@ -90,94 +102,128 @@ export default function UserCreate() {
   }
 
   return (
-    <div>
-      <h1>Create User</h1>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField control={form.control} name="name"
-            render={({ field }) => (
-            <FormItem>
-              <FormLabel>Nome</FormLabel>
-              <FormControl>
-                <Input placeholder="nome completo" {...field} />
-              </FormControl>
-            </FormItem>
-          )} />
-          <FormField control={form.control} name="email"
-            render={({ field }) => (
-            <FormItem>
-              <FormLabel>E-mail</FormLabel>
-              <FormControl>
-                <Input placeholder="e-mail" {...field} />
-              </FormControl>
-            </FormItem>
-          )} />
-          <FormField control={form.control} name="phone_number"
-            render={({ field }) => (
-            <FormItem>
-              <FormLabel>Telefone</FormLabel>
-              <FormControl>
-                <Input placeholder="telefone" {...field} />
-              </FormControl>
-            </FormItem>
-          )} />
-          {/* <FormField control={form.control} name="birthday"
-            render={({ field }) => (
-            <FormItem>
-              <FormLabel>Data de Nascimento</FormLabel>
-              <FormControl>
-                <Input type="date" {...field} />
-              </FormControl>
-            </FormItem>
-          )} /> */}
-          <FormField control={form.control} name="is_smoker"
-            render={({ field }) => (
-            <FormItem>
-              <FormLabel>Fumante</FormLabel>
-              <FormControl>
-                <Switch {...field} />
-              </FormControl>
-            </FormItem>
-          )} />
-          <FormField control={form.control} name="drink_frequency"
-            render={({ field }) => (
-            <FormItem>
-              <FormLabel>Frequência de Bebida</FormLabel>
-              <FormControl>
-                <Select {...field}>
-                  <SelectTrigger>
-                    <SelectValue placeholder="frequência" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectItem value="never">Nunca</SelectItem>
-                    <SelectItem value="sometimes">Às vezes</SelectItem>
-                    <SelectItem value="often">Frequentemente</SelectItem>
-                  </SelectContent>                  
-                </Select>
-              </FormControl>
-            </FormItem>
-          )} />
-          <FormField control={form.control} name="observations"
-            render={({ field }) => (
-            <FormItem>
-              <FormLabel>Observações</FormLabel>
-              <FormControl>
-                <Textarea placeholder="observações" {...field} />
-              </FormControl>
-            </FormItem>
-          )} />
-          <FormField control={form.control} name="accept_tcle"
-            render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Switch {...field} />                
-              </FormControl>
-              <FormLabel>Eu aceito os termos do contrato.</FormLabel>
-            </FormItem>
-          )} />
-          <Button type="submit">Create</Button>
-        </form>
-      </Form>
+    <div className='flex min-h-screen items-center justify-center'>
+      <Card className="w-[600px]">
+        <CardHeader>
+          <CardTitle>Cadastro de Usuário</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField control={form.control} name="name"
+                render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite seu nome" {...field} />
+                  </FormControl>
+                </FormItem>
+              )} />
+              <FormField control={form.control} name="email"
+                render={({ field }) => (
+                <FormItem>
+                  <FormLabel>E-mail</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite seu e-mail" {...field} />
+                  </FormControl>
+                </FormItem>
+              )} />
+              <FormField control={form.control} name="phone_number"
+                render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Telefone</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Digite seu telefone" {...field} />
+                  </FormControl>
+                </FormItem>
+              )} />
+              <FormField control={form.control} name="birthday"
+                render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Data de Nascimento</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant={"outline"}
+                          className={cn(
+                            "w-[240px] pl-3 text-left font-normal",
+                            !field.value && "text-muted-foreground"
+                          )}
+                        >
+                          {field.value ? (
+                            format(field.value, "PPP")
+                          ) : (
+                            <span>Pick a date</span>
+                          )}
+                          <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-auto p-0" align="start">
+                      <Calendar
+                        mode="single"
+                        selected={field.value}
+                        onSelect={field.onChange}
+                        disabled={(date) =>
+                          date > new Date() || date < new Date("1900-01-01")
+                        }
+                        initialFocus
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </FormItem>
+              )} />
+              <FormField control={form.control} name="is_smoker"
+                render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Fumante</FormLabel>
+                  <FormControl>
+                    <Switch {...field} />
+                  </FormControl>
+                </FormItem>
+              )} />
+              <FormField control={form.control} name="drink_frequency"
+                        render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Frequência de Bebida</FormLabel>
+                  <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Selecione a frequência" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="never">Nunca</SelectItem>
+                      <SelectItem value="sometimes">Às vezes</SelectItem>
+                      <SelectItem value="often">Frequentemente</SelectItem>
+                    </SelectContent>                  
+                  </Select>
+                </FormItem>
+              )} />
+              <FormField control={form.control} name="observations"
+                render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Observações</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="observações" {...field} />
+                  </FormControl>
+                </FormItem>
+              )} />
+              <FormField control={form.control} name="accept_tcle"
+                render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Switch {...field} />                
+                  </FormControl>
+                  <FormLabel>Eu aceito os termos do contrato.</FormLabel>
+                </FormItem>
+              )} />
+              <Button type="submit">Create</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
     </div>
   )
 }
