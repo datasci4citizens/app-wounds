fa075d8024ef6b5c100c62cf20c21189f9b45b77
Ricardo Santos <sdric@amazon.com>
2025-05-04 21:51:55 -0300

    refactor qrcode generator

--- src/routes/specialistApp/patient/patient-create/PatientCreateQrCode.tsx
+++ src/routes/specialistApp/patient/patient-create/PatientCreateQrCode.tsx
@@ -14,17 +14,18 @@ export default function PatientCreateQrCode() {
 
   return (
     <div className='container mx-auto flex min-h-screen items-center justify-center p-4'>
-      <Card className='w-full max-w-md'>
-        <CardHeader>
-          <CardTitle className='text-center font-bold text-2xl'>
-            QR Code de Cadastro
-          </CardTitle>
-        </CardHeader>
-        <CardContent className='flex flex-col items-center space-y-4'>
+      <CardContent className="p-8 flex flex-col items-center gap-6">
+          <h1 className="text-xl font-bold text-center">QR code</h1>
+
           <QRCode value={code} size={200} />
-          <h1 className='text-xl font-bold text-center break-words'>{code}</h1>
-        </CardContent>
-      </Card>
+
+          <h1 className='text-l font-bold text-center break-words'>{code}</h1>
+
+          <p className="text-center text-sm text-gray-600">
+            Este código é único e deve ser utilizado apenas uma vez, em até 30 minutos. 
+            Após o uso, ele se tornará inválido.
+          </p>
+      </CardContent>
     </div>
   )
 }
