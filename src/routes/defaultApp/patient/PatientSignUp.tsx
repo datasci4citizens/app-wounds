26d129d30351f0f1390d8a9fed41d634ba5e1ca7
gustavo_mantellatto <g169366@dac.unicamp.br>
2025-05-08 08:43:19 -0300

    estilo tela login paciente

--- src/routes/defaultApp/patient/PatientSignUp.tsx
+++ src/routes/defaultApp/patient/PatientSignUp.tsx
@@ -1,27 +1,38 @@
-import { CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { BandageIcon } from "@/components/ui/new/bandage-logo/BandageIcon";
+import { QrCodeIcon } from "@/components/ui/new/QrCodeIcon";
 import { useNavigate } from "react-router-dom";
-import { QrCode } from "lucide-react";
+import { Button } from "@/components/ui/new/Button";
 
 export default function PatientSignUp() {
   const navigate = useNavigate();
 
   return (
-    <div className='container mx-auto  min-h-screen items-center  p-4'>    
-        <CardContent className="p-8 flex flex-col items-center gap-6">
-          <h1 className="text-xl font-bold text-center">QR code</h1>
-          <p className="text-center text-sm text-gray-600">
-            Para prosseguir com o cadastro, fotografe o código fornecido pelo profissional.
-          </p>
+    <div className="flex flex-col items-center min-h-screen bg-white p-4">
+      <div className="mt-[68px] mb-16">
+        <BandageIcon theme="purple" size={0.5} />
+      </div>
 
-          <div className="border border-gray-200 rounded-md w-40 h-40 flex items-center justify-center">
-            <QrCode className="text-gray-400 w-12 h-12" />
-          </div>
-
-          <Button className="w-full text-white" onClick={() => navigate("/patient-registered")}>
+      <div className="flex flex-col items-center gap-6">
+        <h1 className="text-[#0120AC] text-xl mb-12">QR code</h1>
+        
+        <p className="text-center text-sm text-gray-600 max-w-md p-4">
+          Para prosseguir com o cadastro, fotografe o código fornecido pelo
+          profissional.
+        </p>
+        
+        <div className="flex items-center justify-center mb-4">
+          <QrCodeIcon size={0.8} />
+        </div>
+        
+        <div className="w-full max-w-md flex flex-col items-center">
+          <button
+            className="px-6 py-3 rounded-full bg-[#0120AC] text-white text-sm"
+            onClick={() => navigate("/patient-registered")}
+          >
             Prosseguir
-          </Button>
-        </CardContent>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
