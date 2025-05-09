01c868bcac7ef063febf39a982621271f146fcdf
gustavo_mantellatto <g169366@dac.unicamp.br>
2025-05-08 23:27:58 -0300

    creating wave background layout

---
+++ src/components/ui/new/wave/WaveBackground.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+interface WaveBackgroundProps {
+  className?: string;
+  children?: React.ReactNode;
+  color?: string;
+}
+
+export const WaveBackground: React.FC<WaveBackgroundProps> = ({ 
+  className = '', 
+  children,
+  color = "#D0DCFF"
+}) => {
+  return (
+    <div className={`relative w-full ${className}`}>
+      <svg 
+        width="100%" 
+        height="29" 
+        viewBox="0 0 430 29" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none"
+        className="w-full"
+      >
+        <path 
+          d="M0 0H430C430 16.0163 417.016 29 401 29H29C12.9838 29 0 16.0163 0 0Z" 
+          fill={color} 
+        />
+      </svg>
+      {children}
+    </div>
+  );
+};
+
+// A version that can be used as a page background with the wave at the top
+export const WaveBackgroundLayout: React.FC<WaveBackgroundProps> = ({ 
+  className = '', 
+  children,
+  color = "#D0DCFF"
+}) => {
+  return (
+    <div className={`min-h-screen bg-background ${className}`}>
+      <svg 
+        width="100%" 
+        height="29" 
+        viewBox="0 0 430 29" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none"
+        className="w-full"
+      >
+        <path 
+          d="M0 0H430C430 16.0163 417.016 29 401 29H29C12.9838 29 0 16.0163 0 0Z" 
+          fill={color} 
+        />
+      </svg>
+      <div className="container mx-auto p-4">
+        {children}
+      </div>
+    </div>
+  );
+};
+
