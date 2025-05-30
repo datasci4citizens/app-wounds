5191c5d3ca49cfd4e0a1b6e90dc9b71696321aa8
gustavo_mantellatto <g169366@dac.unicamp.br>
2025-05-30 01:01:54 -0300

    creating load screen for api calls

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,10 +38,15 @@ export default {
 					from: { height: 'var(--radix-accordion-content-height)' },
 					to: { height: '0' },
 				},
+				loadingBar: {
+					'0%': { transform: 'translateX(-100%)' },
+					'100%': { transform: 'translateX(100%)' },
+				},
 			},
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out',
+				'loading-bar': 'loadingBar 1.5s ease-in-out infinite',
 			},
 			colors: {
 				border: stone[200],
