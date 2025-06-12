7ac527d65b4b66b4e8388b8230a218ff05b7c37b
TheDeas343 <deascisi@hotmail.com>
2025-06-12 16:44:54 -0300

    Finishing Menu Design

--- src/routes/patientApp/Menu.tsx
+++ src/routes/patientApp/Menu.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import { SearchBar } from '../../components/shared/SearchBar.tsx'
 import { WaveBackgroundLayout } from '@/components/ui/new/wave/WaveBackground.tsx'
 import CategoryCard from '@/components/ui/new/card/CategoryCard.tsx'
 import { MenuMobile } from '@/components/ui/new/menu/MenuMobile.tsx'
@@ -14,11 +13,8 @@ interface AuthMeResponse {
   email: string;
 }
 
-//Testing 
-
 export default function Menu() {
   const navigate = useNavigate()
-  const [searchQuery, setSearchQuery] = useState('')
   const [patientName, setPatientName] = useState('')
 
   useEffect(() => {
@@ -74,10 +70,6 @@ export default function Menu() {
   const handleNewCaseClick = () => navigate('/patient/wounds');
   const handleNotificationsClick = () => navigate('/patient/notifications');
 
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value)
-  }
-
   return (
     <WaveBackgroundLayout className="bg-[#F9FAFB]">
       <div className="flex justify-center items-center mt-6 mb-6">
@@ -89,15 +81,6 @@ export default function Menu() {
         </h1>
       </div>
       
-      <div className="mb-6 w-full mt-8 px-4 md:px-6 lg:px-8">
-        <SearchBar 
-          placeholder="" 
-          value={searchQuery} 
-          onChange={handleSearch} 
-          onAddClick={() => handleNavigate('/create')}
-          height="64px"
-        />
-      </div>
       
       <div className="px-4 md:px-6 lg:px-8 mb-8 mt-4">
         <h2 className="text-[#0120AC] text-md font-semibold text-left">Categorias</h2>
