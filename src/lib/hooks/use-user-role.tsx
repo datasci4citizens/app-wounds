09d7c947f40604ad92cd6ecdc1bd1c888abd9f88
Théo Giovedi <tmgiovedi@gmail.com>
2026-03-31 18:17:06 -0300

    clean: remove unnecessary old files

--- src/lib/hooks/use-user-role.tsx
+++
@@ -1,6 +0,0 @@
-export function useUserRole(): "patient" | "specialist" | null {
-    const stored = localStorage.getItem("user_role");
-    if (stored === "patient" || stored === "specialist") return stored;
-    return null;
-  }
-  
